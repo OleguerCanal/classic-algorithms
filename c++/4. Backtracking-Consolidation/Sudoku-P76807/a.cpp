0f69a037f4623b86b7716641ded77a5ae8fc5101
oleguer <oleguer.canal@hotmail.com>
2019-07-07 17:23:26 +0200

    Worked on sudoku

--- c++/4. Backtracking-Consolidation/Sudoku-P76807/a.cpp
+++ c++/4. Backtracking-Consolidation/Sudoku-P76807/a.cpp
@@ -20,23 +20,19 @@ const int size = 9;
 // instead of iterate over sudolu
 class Sudoku {
  public:
-  mi values;
-  mb rows;
-  mb cols;
-  mb squares;
-
   Sudoku() {
-    values = mi(size, vi(size, -1));
-    rows = mb(size, vb(size, false));
-    cols = mb(size, vb(size, false));
-    squares = mb(size, vb(size, false));
+    values_ = mi(size, vi(size, -1));
+    rows_ = mb(size, vb(size, false));
+    cols_ = mb(size, vb(size, false));
+    squares_ = mb(size, vb(size, false));
   }
+  ~Sudoku() {}
 
   // Whether the sudoku is solved
   bool IsSolved() {  // TODO(oleguer): Could be optimized
     for (int i = 0; i < size; ++i)
       for (int j = 0; j < size; ++j)
-        if (!rows[i][j] || !cols[i][j] || !squares[i][j]) return false;
+        if (!rows_[i][j] || !cols_[i][j] || !squares_[i][j]) return false;
     return true;
   }
 
@@ -49,26 +45,24 @@ class Sudoku {
 
   // Mark value val in position i, j
   void InputValue(const int &val, const int &i, const int &j) {
-    std::cout << "i:" << i << ", j:" << j << ", sq:" << GetSquare(i, j)
-              << ", v:" << val << std::endl;
-    values[i][j] = val;
-    std::cout << values[i][j] << std::endl;
-    // Print();
+    // std::cout << "i:" << i << ", j:" << j << ", sq:" << GetSquare(i, j)
+    //           << ", v:" << val << std::endl;
+    values_[i][j] = val;
     int v = val - 1;
-    rows[i][v] = true;
-    cols[j][v] = true;
-    squares[GetSquare(i, j)][v] = true;
+    rows_[i][v] = true;
+    cols_[j][v] = true;
+    squares_[GetSquare(i, j)][v] = true;
   }
 
   // Assume that the value was correctly placed
   void RemoveValue(const int &i, const int &j) {
-    int val = values[i][j];
-    values[i][j] = -1;
+    int val = values_[i][j];
+    values_[i][j] = -1;
     if (val != -1) {
       int v = val - 1;
-      rows[i][v] = false;
-      cols[j][v] = false;
-      squares[GetSquare(i, j)][v] = false;
+      rows_[i][v] = false;
+      cols_[j][v] = false;
+      squares_[GetSquare(i, j)][v] = false;
     }
   }
 
@@ -76,12 +70,12 @@ class Sudoku {
   bool IsPossible(const int &val, const int &i, const int &j) {
     // row, col, sqare val must be false (it cant be in any of these positions)
     int v = val - 1;
-    if (values[i][j] != -1) return false;
-    return !rows[i][v] && !cols[j][v] && !squares[GetSquare(i, j)][v];
+    if (values_[i][j] != -1) return false;
+    return !rows_[i][v] && !cols_[j][v] && !squares_[GetSquare(i, j)][v];
   }
 
   // Wheter there is already a number at position i, j
-  bool IsEmpty(const int &i, const int &j) { return (values[i][j] != -1); }
+  bool IsEmpty(const int &i, const int &j) { return (values_[i][j] == -1); }
 
   // IO functions
   // Read from statement
@@ -90,15 +84,13 @@ class Sudoku {
     for (int i = 0; i < size; ++i)
       for (int j = 0; j < size; ++j) {
         std::cin >> c;
-        // std::cout << c << std::endl;
         if (c == '.')
-          values[i][j] = -1;
+          values_[i][j] = -1;
         else {
           int val = c - '0';
           InputValue(val, i, j);
         }
       }
-    // Print();
   }
 
   // Print current sudoku
@@ -106,11 +98,18 @@ class Sudoku {
     std::cout << std::endl;
     for (int i = 0; i < size; ++i) {
       for (int j = 0; j < size; ++j) {
-        std::cout << values[i][i] << " ";  // TODO(oleguer): Probably wrong
+        std::cout << values_[i][j] << " ";  // TODO(oleguer): Probably wrong
       }
       std::cout << std::endl;
     }
   }
+
+ private:
+  mi values_;
+  mb rows_;
+  mb cols_;
+  mb squares_;
+    // std::cout << "i:" << i << ", j:" << j << std::endl;
 };
 
 Coord GetCoord(const int &pos) {
@@ -120,7 +119,7 @@ Coord GetCoord(const int &pos) {
 }
 
 void SolveSudoku(const int &pos, Sudoku *sudoku) {
-  if (pos == size * size - 1) {
+  if (pos == size * size) {
     if (sudoku->IsSolved()) sudoku->Print();
     return;
   }
@@ -131,15 +130,17 @@ void SolveSudoku(const int &pos, Sudoku *sudoku) {
 
   bool tried_value = false;
   if (sudoku->IsEmpty(i, j)) {
-    for (int val = 0; val < size; ++val)
+    for (int val = 1; val <= size; ++val)
       if (sudoku->IsPossible(val, i, j)) {
         tried_value = true;
         sudoku->InputValue(val, i, j);
         SolveSudoku(pos + 1, sudoku);
         sudoku->RemoveValue(i, j);
       }
+    if (!tried_value) return;  // If no value is possible
+  } else {  // If there is a value, skip
+    SolveSudoku(pos + 1, sudoku);
   }
-  if (!tried_value) return;  // If no value is possible
 }
 
 int main() {
@@ -149,8 +150,6 @@ int main() {
   for (int i = 0; i < n; ++i) {
     Sudoku sudoku;
     sudoku.ReadSudoku();
-    std::cout << i << std::endl;
-    sudoku.Print();
     SolveSudoku(0, &sudoku);
   }
 }
