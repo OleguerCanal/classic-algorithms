bed203cd7ea053c34767d30d36e121da5d99a8ba
OleguerCanal <oleguer.canal@hotmail.com>
2019-08-30 20:24:43 +0200

    First approach

--- c++/Artificial Intelligence Course KTH/Sokoban_Pathfinding/a.cpp
+++ c++/Artificial Intelligence Course KTH/Sokoban_Pathfinding/a.cpp
@@ -1,11 +1,12 @@
+// Soku pathfinding
+#include <stdlib.h>
+
 #include <iostream>
 #include <map>
 #include <string>
 #include <vector>
 
 namespace sokoban {
-typedef std::vector<std::vector<bool>> vvb;
-typedef std::vector<std::vector<int>> vvi;
 typedef std::pair<int, int> position;
 
 enum class Solvable { solved, impossible, unknown };  // Solvability of the game
@@ -13,12 +14,31 @@ const std::map<char, std::string> map_elements = {
     {' ', "EMPTY"}, {'#', "WALL"}, {'.', "GOAL"},      {'@', "PLAYER"},
     {'+', "DONE"},  {'$', "WALL"}, {'*', "IMPOSSIBLE"}};
 
+struct Node {
+  position pos, parent_pos;
+  std::vector<position> child_pos;
+  int g, h = 0;
+
+  int f() { return g + h; }
+
+  // Returns Manhattan distance between passed and this node
+  int ComputeDistance(const Node& node2) {
+    h = std::abs(pos.first - node2.pos.first) +
+        std::abs(pos.second - node2.pos.second);
+  }
+};
+
 class Game {
  public:
   Game(const int& size_x, const int& size_y) {
     size_x_ = size_x;
     size_y_ = size_y;
-    vvi graph = std::vector(size_x * size_y, std::vector<int>(0));
+    std::vector<std::vector<Node>> graph =
+        std::vector(size_x, std::vector<Node>(size_y));
+    for (size_t i = 0; i < graph.size(); i++)
+      for (size_t j = 0; j < graph[0].size(); j++) {
+
+      }
     graph_ = graph;
   }
   ~Game() {}
@@ -26,7 +46,8 @@ class Game {
   // Populates map, player_pos and objective_pos
   // Returns either solved, impossible or unknown solvability
   Solvable ReadMap() {
-    vvb map = vvb(size_x_, std::vector<bool>(size_y_, false));
+    std::vector<std::vector<bool>> map = std::vector<std::vector<bool>>(
+        size_x_, std::vector<bool>(size_y_, false));
     for (size_t i = 0; i < map.size(); i++) {
       for (size_t j = 0; j < map[0].size(); j++) {
         char c;
@@ -34,8 +55,8 @@ class Game {
         std::string element = map_elements[c];
         if (element == "EMPTY" || element == "GOAL" || element == "PLAYER") {
           map[i][j] = true;
-          if (element == "GOAL") goal_pos_ = position(i, j);
-          if (element == "PLAYER") player_pos_ = position(i, j);
+          if (element == "GOAL") start_pos_ = position(i, j);
+          if (element == "PLAYER") goal_pos_ = position(i, j);
         }
         if (element == "DONE") return Solvable::solved;
         if (element == "IMPOSSIBLE") return Solvable::impossible;
@@ -45,46 +66,37 @@ class Game {
     return Solvable::unknown;
   }
 
-  // Solvable Find
+  Solvable FindPath(std::string* solution) {}
 
  private:
-    // Convert (i,j) pos to mapped CellId
-  int PosToCellId(const position& pos) {
-    return pos.first * size_y_ + pos.second;
-  }
-
-  // Convert mapped CellId to (i,j) pos
-  position CellIdToPos(const int& cellid) {
-    return position(cellid / size_y_, cellid % size_y_);
-  }
-
-  // Returns distance between 2 positions
-  int Distance(const position& pos_1, const position& pos_2) {
-    return (pos_1.first - pos_2.first) * (pos_1.first - pos_2.first) +
-           (pos_1.second - pos_2.second) * (pos_1.second - pos_2.second);
-  }
-  
-  
   // Populates graph_ private variable
   void BuildGraph() {
+    // Get goal node to pre-compute euristics
+    Node* goal_node = &graph_[goal_pos_.first][goal_pos_.second];
+    goal_node->ComputeDistance(*goal_node);
+
     int x_dif[4] = {1, -1, 0, 0};
     int y_dif[4] = {0, 0, 1, -1};
+    char movement = {"R", }
     for (size_t i = 0; i < map_.size(); i++)
       for (size_t j = 0; j < map_[0].size(); j++)  // Each elem of map
         if (map_[i][j]) {                          // If its walkable
-          int current_cell_id = PosToCellId(position(i, j));
-          for (size_t a = 0; a < 4; a++)
+          // Fill node info (h, child_ids)
+          Node* current_node = &graph_[i][j];
+          current_node->ComputeDistance(*goal_node);  // Aprox distance to goal
+          for (size_t a = 0; a < 4; a++)  // Childs
             if (map_[i + x_dif[a]]
-                    [j + y_dif[a]])  // If walkable append as connected
-              graph_[current_cell_id].push_back(
-                  PosToCellId(position(i + x_dif[a], j + y_dif[a])));
+                    [j + y_dif[a]]) {  // If walkable append as connected
+              current_node->child_pos.push_back(
+                  position(i + x_dif[a], j + y_dif[a]));
+            }
         }
   }
 
   int size_x_, size_y_;  /// Shape of map
-  vvb map_;              /// 1 == walkable path, 0 == blocked
-  position player_pos_, goal_pos_;
-  vvi graph_;  /// Each element contains a vector of the connected nodes
+  position start_pos_, goal_pos_;         /// Ids of the start and end nodes
+  std::vector<std::vector<bool>> map_;    /// 1 == walkable path, 0 == blocked
+  std::vector<std::vector<Node>> graph_;  // Stores node info
 };
 
 };  // namespace sokoban
